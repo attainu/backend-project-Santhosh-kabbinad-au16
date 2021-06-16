68f4fd47da8f0238d35919071f6c4a3369b7929b
rutuja chandekar <rutuja-chandekar-au16>
2021-06-16 17:06:49 +0530

    adding FE pages

---
+++ home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+    return(
+        <>
+            <div className="main_div">
+                <div className="center_div">
+                    <br />
+                    <h1> watch list</h1>
+                    <br />
+                    <input type="text" placeholder="add items" />
+                    <button> + </button>
+                </div>
+            </div> 
+        </>
+           
+    );
+};
+
+
